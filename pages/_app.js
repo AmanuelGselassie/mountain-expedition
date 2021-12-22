812ebd47f455e5caca5bf0c65c566f8bd2b7b276
Amanuel Gebreselassie <amanuel.gselassie99@gmail.com>
2021-12-22 11:13:59 +0800

    typo error

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
-import '../styles/home.module.css';
+import '../styles/Home.module.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
